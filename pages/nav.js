87486d72e987a1de26aeecb753e68633a98c11eb
Ben Doyle <benjaminmarkdoyle@gmail.com>
2022-11-17 13:56:09 -0700

    bootstrap setup

---
+++ pages/nav.js
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+export default function Nav() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" href="#">The Lodge</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <Link class="nav-link active" aria-current="page" href="#">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" href="#">Link</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class='d-flex'>
+                        <Link class="nav-link" href="#">Sign in</Link>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
