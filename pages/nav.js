d72632aadf0e78f3b3e1e806fc4ad805fa0e4132
Ben Doyle <benjaminmarkdoyle@gmail.com>
2022-11-17 14:05:30 -0700

    updated navbar

--- pages/nav.js
+++ pages/nav.js
@@ -5,11 +5,11 @@ export default function Nav() {
         <div>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="#">The Lodge</Link>
+                    <Link className="navbar-brand" href="#">The Lodge at 1818 North</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" href="#">Home</Link>
@@ -18,7 +18,7 @@ export default function Nav() {
                                 <Link className="nav-link" href="#">Link</Link>
                             </li>
                         </ul>
-                    </div>
+                    </div> */}
                     <div className='d-flex'>
                         <Link className="nav-link" href="#">Sign in</Link>
                     </div>
