2a46008fb628ed240231cc03971edabdc303b3fd
Ben Doyle <benjaminmarkdoyle@gmail.com>
2022-11-17 14:12:28 -0700

    added dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard(props) {
+    return (
+        <div className='vh-100'>
+            <h2>{props.message}</h2>
+        </div>
+    )
+}
--- pages/nav.js
+++ pages/nav.js
@@ -6,9 +6,9 @@ export default function Nav() {
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <Link className="navbar-brand" href="#">The Lodge at 1818 North</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </button> */}
                     {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
