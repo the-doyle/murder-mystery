47a31853f1af847fbf5be1b2640916c1e15c4249
Ben Doyle <benjaminmarkdoyle@gmail.com>
2022-11-17 14:01:14 -0700

    fixed script

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 import Nav from './nav'
 
@@ -16,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Nav></Nav>
-        <Link src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></Link>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></Script>
       </main>
 
       <footer className={styles.footer}>
