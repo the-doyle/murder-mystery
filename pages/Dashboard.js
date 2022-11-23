f7c3c9e7d649b038015476837563156ad69c968b
Ben Doyle <benjaminmarkdoyle@gmail.com>
2022-11-23 13:53:35 -0700

    fixes

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -15,10 +15,10 @@ export default function Dashboard() {
     const [itemCode, setItemCode] = useState()
     const [roomCode, setRoomCode] = useState()
 
-    const[weapons, setWeapons] = useState()
-    const[diversions, setDiversions] = useState()
-    const[targets, setTargets] = useState() 
-    const[murders, setMurders] = useState() 
+    const[weapons, setWeapons] = useState(null)
+    const[diversions, setDiversions] = useState(null)
+    const[targets, setTargets] = useState(null) 
+    const[murders, setMurders] = useState(null) 
 
     const refreshCharacter = () => {
         GetCharacter()
@@ -140,7 +140,7 @@ export default function Dashboard() {
                                 </form>
                             </div>
 
-                            {weapons && diversions && targets && player
+                            {weapons && weapons.length > 0 && diversions && diversions.length > 0 && targets && targets.length > 0 && player
                                 ?   <Murder 
                                         player={player} 
                                         weapons={weapons} 
--- pages/Murder.js
+++ pages/Murder.js
@@ -56,7 +56,7 @@ export default function Murder(props) {
         }
     }
 
-    return (
+    return !props.diversions ? null : (
         <div>
             <div className='col-12 mt-5'>
                 <button type="button" className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#murder" disabled={!murderEnabled}>
